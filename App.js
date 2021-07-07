ba1dbe211781c36df534db4e902c418147ed23b6
Aneta590 <anetalisiecka.al@gmail.com>
2021-07-07 14:39:17 +0100

    Flatlist

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, FlatList, View, Text } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state={
+      countries:[]
+    }
+
+    this.renderItem = this.renderItem.bind(this);
+  }
+
+  async componentDidMount(){
+    console.log('infos')
+    try{
+    const info = await fetch('http://restcountries.eu/rest/v2/all')
+    console.log(info)
+    const result= await info.json()
+    console.log(result)
+    this.setState({countries:result})
+  }catch(error){
+    console.log(error)
+  }
+} 
+
+  renderItem({ item }) {
+    return (
+      <View>
+        <Text style={st.container}>{item.capital}</Text>
+        <Image
+  source={{ uri:item.flag}}
+  style={{ width: 100, height: 100, marginLeft:80 }} />
+      </View>
+      
+    );
+  }
+  
+
+  render() { 
+    
+    return (
+     
+      <FlatList
+        data={this.state.countries} 
+        renderItem={this.renderItem}
+        keyExtractor={(item, index) => index.toString()}      
+        />
+       
+    );
+  }
 }
+const st= StyleSheet.create({
+  container:{
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "blue",
+    textAlign:'center'
+  }
+})
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
